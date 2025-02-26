4774ba79c798a74002e7c6cae1db13cecf5ac8cd
Igor Ferraz <igorsousaferrazaraga2@gmail.com>
2025-02-26 20:00:09 -0300

    Segundo Commit
    Registrando segundo commit.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Antes do primeiro commit.");
+        System.out.println("Antes do segundo commit.");
     }
 }
